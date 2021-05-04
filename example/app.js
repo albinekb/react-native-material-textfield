6ec58dc6f8f382e27d8c36f50d875ed96349e23c
Albin Ekblom <albinekblom@gmail.com>
2021-05-04 10:59:30 +0200

    Stop using value as fn

--- example/app.js
+++ example/app.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   ScrollView,
   View,
   SafeAreaView,
   Platform,
-} from 'react-native';
-import { RaisedTextButton } from 'react-native-material-buttons';
-import { TextField } from 'react-native-material-textfield';
-import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+} from 'react-native'
+import { RaisedTextButton } from 'react-native-material-buttons'
+import { TextField } from 'react-native-material-textfield'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 
 let styles = {
   scroll: {
@@ -34,121 +34,121 @@ let styles = {
     flex: 1,
     backgroundColor: '#E8EAF6',
   },
-};
+}
 
 let defaults = {
   firstname: 'Eddard',
   lastname: 'Stark',
-  about: 'Stoic, dutiful, and honorable man, considered to embody the values of the North',
-};
+  about:
+    'Stoic, dutiful, and honorable man, considered to embody the values of the North',
+}
 
 export default function init() {
   class Example extends Component {
     constructor(props) {
-      super(props);
+      super(props)
 
-      this.onFocus = this.onFocus.bind(this);
-      this.onSubmit = this.onSubmit.bind(this);
-      this.onChangeText = this.onChangeText.bind(this);
-      this.onSubmitFirstName = this.onSubmitFirstName.bind(this);
-      this.onSubmitLastName = this.onSubmitLastName.bind(this);
-      this.onSubmitAbout = this.onSubmitAbout.bind(this);
-      this.onSubmitEmail = this.onSubmitEmail.bind(this);
-      this.onSubmitPassword = this.onSubmitPassword.bind(this);
-      this.onAccessoryPress = this.onAccessoryPress.bind(this);
+      this.onFocus = this.onFocus.bind(this)
+      this.onSubmit = this.onSubmit.bind(this)
+      this.onChangeText = this.onChangeText.bind(this)
+      this.onSubmitFirstName = this.onSubmitFirstName.bind(this)
+      this.onSubmitLastName = this.onSubmitLastName.bind(this)
+      this.onSubmitAbout = this.onSubmitAbout.bind(this)
+      this.onSubmitEmail = this.onSubmitEmail.bind(this)
+      this.onSubmitPassword = this.onSubmitPassword.bind(this)
+      this.onAccessoryPress = this.onAccessoryPress.bind(this)
 
-      this.firstnameRef = this.updateRef.bind(this, 'firstname');
-      this.lastnameRef = this.updateRef.bind(this, 'lastname');
-      this.aboutRef = this.updateRef.bind(this, 'about');
-      this.emailRef = this.updateRef.bind(this, 'email');
-      this.passwordRef = this.updateRef.bind(this, 'password');
-      this.houseRef = this.updateRef.bind(this, 'house');
+      this.firstnameRef = this.updateRef.bind(this, 'firstname')
+      this.lastnameRef = this.updateRef.bind(this, 'lastname')
+      this.aboutRef = this.updateRef.bind(this, 'about')
+      this.emailRef = this.updateRef.bind(this, 'email')
+      this.passwordRef = this.updateRef.bind(this, 'password')
+      this.houseRef = this.updateRef.bind(this, 'house')
 
-      this.renderPasswordAccessory = this.renderPasswordAccessory.bind(this);
+      this.renderPasswordAccessory = this.renderPasswordAccessory.bind(this)
 
       this.state = {
         secureTextEntry: true,
         ...defaults,
-      };
+      }
     }
 
     onFocus() {
-      let { errors = {} } = this.state;
+      let { errors = {} } = this.state
 
       for (let name in errors) {
-        let ref = this[name];
+        let ref = this[name]
 
         if (ref && ref.isFocused()) {
-          delete errors[name];
+          delete errors[name]
         }
       }
 
-      this.setState({ errors });
+      this.setState({ errors })
     }
 
     onChangeText(text) {
-      ['firstname', 'lastname', 'about', 'email', 'password']
+      ;['firstname', 'lastname', 'about', 'email', 'password']
         .map((name) => ({ name, ref: this[name] }))
         .forEach(({ name, ref }) => {
           if (ref.isFocused()) {
-            this.setState({ [name]: text });
+            this.setState({ [name]: text })
           }
-        });
+        })
     }
 
     onAccessoryPress() {
-      this.setState(({ secureTextEntry }) => ({ secureTextEntry: !secureTextEntry }));
+      this.setState(({ secureTextEntry }) => ({
+        secureTextEntry: !secureTextEntry,
+      }))
     }
 
     onSubmitFirstName() {
-      this.lastname.focus();
+      this.lastname.focus()
     }
 
     onSubmitLastName() {
-      this.about.focus();
+      this.about.focus()
     }
 
     onSubmitAbout() {
-      this.email.focus();
+      this.email.focus()
     }
 
     onSubmitEmail() {
-      this.password.focus();
+      this.password.focus()
     }
 
     onSubmitPassword() {
-      this.password.blur();
+      this.password.blur()
     }
 
     onSubmit() {
-      let errors = {};
+      let errors = {}
 
-      ['firstname', 'lastname', 'email', 'password']
-        .forEach((name) => {
-          let value = this[name].value();
+      ;['firstname', 'lastname', 'email', 'password'].forEach((name) => {
+        let value = this[name].value
 
-          if (!value) {
-            errors[name] = 'Should not be empty';
-          } else {
-            if ('password' === name && value.length < 6) {
-              errors[name] = 'Too short';
-            }
+        if (!value) {
+          errors[name] = 'Should not be empty'
+        } else {
+          if ('password' === name && value.length < 6) {
+            errors[name] = 'Too short'
           }
-        });
+        }
+      })
 
-      this.setState({ errors });
+      this.setState({ errors })
     }
 
     updateRef(name, ref) {
-      this[name] = ref;
+      this[name] = ref
     }
 
     renderPasswordAccessory() {
-      let { secureTextEntry } = this.state;
+      let { secureTextEntry } = this.state
 
-      let name = secureTextEntry?
-        'visibility':
-        'visibility-off';
+      let name = secureTextEntry ? 'visibility' : 'visibility-off'
 
       return (
         <MaterialIcon
@@ -158,16 +158,16 @@ export default function init() {
           onPress={this.onAccessoryPress}
           suppressHighlighting={true}
         />
-      );
+      )
     }
 
     render() {
-      let { errors = {}, secureTextEntry, ...data } = this.state;
-      let { firstname, lastname } = data;
+      let { errors = {}, secureTextEntry, ...data } = this.state
+      let { firstname, lastname } = data
 
       let defaultEmail = `${firstname || 'name'}@${lastname || 'house'}.com`
         .replace(/\s+/g, '_')
-        .toLowerCase();
+        .toLowerCase()
 
       return (
         <SafeAreaView style={styles.safeContainer}>
@@ -269,9 +269,9 @@ export default function init() {
             </View>
           </ScrollView>
         </SafeAreaView>
-      );
+      )
     }
   }
 
-  AppRegistry.registerComponent('example', () => Example);
+  AppRegistry.registerComponent('example', () => Example)
 }
